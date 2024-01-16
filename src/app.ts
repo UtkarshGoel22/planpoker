b7f40569f0acaad57ef05260b3643a3f1acc690f
UtkarshGoel22 <utkarsh.goel22@gmail.com>
2024-01-16 13:18:09 +0530

    Add global catch.

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import cors from 'cors';
-import express from 'express';
+import express, { NextFunction, Response, Request } from 'express';
+import { StatusCodes } from 'http-status-codes';
 
 import { Routes } from './constants/enums';
+import { ErrorMessages } from './constants/message';
 import pokerboardRouter from './routes/pokerboard.route';
 import userRouter from './routes/user.route';
 import usersRouter from './routes/users.route';
 import config from './settings/config';
+import { makeResponse } from './utils/common';
 
 class App {
   public app: express.Application;
@@ -23,6 +26,13 @@ class App {
     this.app.use(Routes.USER, userRouter);
     this.app.use(Routes.USERS, usersRouter);
     this.app.use(Routes.POKERBOARD, pokerboardRouter);
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    this.app.use((err, req: Request, res: Response, next: NextFunction) => {
+      const errorData = { somethingWentWrong: ErrorMessages.SOMETHING_WENT_WRONG };
+      res
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .json(makeResponse(false, ErrorMessages.SOMETHING_WENT_WRONG, errorData));
+    });
   }
 }
 
